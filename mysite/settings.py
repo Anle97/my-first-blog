72734469fdb98746cbf694be4cbb3f0a102c6a80
An <an.tle.914@gmail.com>
2022-06-28 09:00:04 -0400

    adding anle97.pythonanywhere.com to allowed_host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lcqrs_@1_0%cdu%%dqhu_=e$=1&&oq+lk(b1bc-^^^m4_rsyyq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', 'anle97.pythonanywhere.com']
 
 
 # Application definition
